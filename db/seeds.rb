7e765d6b7320feb27546dcc85e2c27d20fde0cea
gwendal le bris <gwen.coding@gmail.com>
2023-05-19 20:24:18 +0200

    Do Livecode

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Destroying Database...'
+
+Garden.destroy_all
+
+puts 'Database cleaned !'
+
+puts 'Creating Gardens...'
+
+Garden.create!(
+  name: 'My Little Garden',
+  banner_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg'
+)
+
+Garden.create!(
+  name: 'My Other Garden',
+  banner_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg'
+)
+
+puts "#{Garden.count} Gardens created !"
